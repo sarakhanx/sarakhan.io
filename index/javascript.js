9710301b2133d7a0b8c7038033fe17accc8e5d3f
sarakhanx <sarawut.khan@hotmail.com>
2022-01-03 17:20:22 +0700

    first commit

---
+++ index/javascript.js
@@ -0,0 +1,16 @@
+function whoWeighter(){
+function weight (pweight){
+    let standard = 10;
+    return pweight+standard;
+}
+    let golf = weight(90);
+    let amp = weight(50);
+if(golf>=amp){
+    console.log("กอล์ฟน้ำหนัก= ",golf);
+    }else{
+        console.log("แอ๋มน้ำหนัก= ",amp);
+    }
+}
+whoWeighter();
+whoWeighter();
+whoWeighter();
